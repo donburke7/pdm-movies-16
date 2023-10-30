57c13fc807105bafdc4e4d348a5b3c4470f74569
sa7936 <sa7936@rit.edu>
2023-10-29 20:12:32 -0400

    with the actual comments

---
+++ src/accounts.java
@@ -0,0 +1,7 @@
+/// create account has to check if a user exists
+/// if not, create account , if does ask to login
+/// when creating acc. check if username exists/not
+/// store last accessed into table
+/// store creation date into table
+/// everytime someone signs in update last accessed
+
