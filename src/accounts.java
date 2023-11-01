23f179ef0d292568dcece9790e5738901c34f3a6
sa7936 <sa7936@rit.edu>
2023-10-31 21:52:22 -0400

    Stubbed out sql statements

--- src/accounts.java
+++ src/accounts.java
@@ -1,3 +1,8 @@
+import java.util.Scanner;
+import java.sql.*;
+
+import javax.swing.text.Utilities;
+
 /// create account has to check if a user exists
 /// if not, create account , if does ask to login
 /// when creating acc. check if username exists/not
@@ -5,3 +10,55 @@
 /// store creation date into table
 /// everytime someone signs in update last accessed
 
+public class accounts {
+    public static Scanner sc = new Scanner(System.in);
+    public static int userID = -1;
+    public static int incrementUserID = 1001;
+    public boolean isLogin(){
+        if (userID!= -1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static void Login(){
+      
+        System.out.println("Enter username:");
+        String username = sc.nextLine();
+        System.out.println("Enter password:");
+        String password = sc.nextLine();
+        int tempID;
+        // tempID = (SELECT "userID" from "Users"
+        //             WHERE ("username" = username) AND
+        //             ("password" = password))
+        // if (tempID != null){
+        //     userID = tempID;
+        //     Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+        //     UPDATE "Users" SET lastAccess = currentTime
+        //     where userID = userID
+        // }
+        // else{
+        //     System.out.println("Could not login - try again or create an account");
+        // }
+    }
+
+    public static void createAccount(){
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+        System.out.println("Please enter first name:");
+        String firstName = sc.nextLine();
+        System.out.println("Please enter last name:");
+        String lastName = sc.nextLine();
+        System.out.println("Please enter new username:");
+        String username = sc.nextLine();
+        System.out.println("Please enter new password:");
+        String password = sc.nextLine();
+        
+        // INSERT INTO "Users" VALUES 
+        // "(incrementUserID, username, password, firstName, lastName, currentTime, null);
+
+        // incrementUserID++;
+
+    }
+}
