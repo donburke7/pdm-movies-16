a86e52b2277bedca10589bc6a3253bac7a8c9f11
sa7936 <sa7936@rit.edu>
2023-11-01 21:22:22 -0400

    Fixing incrementing uids now

--- src/main/java/accounts.java
+++ src/main/java/accounts.java
@@ -11,20 +11,49 @@ import java.util.Properties;
 
 import javax.swing.text.Utilities;
 
-/// create account has to check if a user exists
-/// if not, create account , if does ask to login
-/// when creating acc. check if username exists/not
-/// store last accessed into table
-/// store creation date into table
-/// everytime someone signs in update last accessed
 
-public class accounts {
+/// This class handles all account issues as well as passing off functionality to other 
+/// classes
+public class Accounts {
+    //The scanner used for reading user input
     public static Scanner sc = new Scanner(System.in);
-    public static int userID = -1;
-    public static int incrementUserID = 1001;
-    static Statement stmt;
-    public static boolean loginChecker;
 
+    // the static userID of the current user logged in
+    public static int userID = -1; 
+
+    // the counter to increment generated userIDs when creating accounts
+    public static int incrementUserID;
+
+    // used for sql statements
+    static Statement stmt;
+
+    // checks if a user is logged in or not
+    public static boolean loginChecker;
+    
+    /**
+     * Constructor to put userID to be used for creating account in
+     */
+    public Accounts(){
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("src/incrementUserID.txt"))) {
+            incrementUserID = Integer.parseInt(bufferedReader.readLine());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    /**
+     * This gets the current logged in user's userID 
+     * @return the int userID
+     */
+    public static int getUserID(){
+        return userID;
+    }
+
+    public static void incrementCounterUserID(){
+        incrementUserID = incrementUserID+1;
+    }
+    /**
+     * This prints the beginning menu when a user launches console app.
+     */
     public static void printBeginMenu(){
         System.out.println("Welcome to the Movies Database!");
         System.out.println("Please sign in or create account with commands: \n");
@@ -53,7 +82,9 @@ public class accounts {
         }
         
     }
-
+    /**
+     * This prints the main menu after a user has logged in and directs them to the right path.
+     */
     public static void printMainMenu(){
         System.out.println("You are now logged in! Find below more functionality:\n");
 
@@ -63,11 +94,35 @@ public class accounts {
         System.out.println("5: Watch Movies");
         System.out.println("6: Follow other Users");
         System.out.println("7: Logout");
-
-
+        int input = Integer.parseInt(sc.nextLine());
+        while(input != 2 && input != 3 && input != 4 && input != 5 && input != 6 && input != 7){
+            System.out.println("Please enter valid number of command you wish to perform: \n");
+            input = Integer.parseInt(sc.nextLine());
+        }
+        if(input == 2){
+            //collection work
+        }
+        if(input == 3){
+            //search work
+        }
+        if(input == 4){
+            //rate work
+        }
+        if(input == 5){
+            //watch work
+        }
+        if(input == 6){
+            //followers work
+        }
+        else if(input == 7){
+            Logout();
+        }
     }
 
-
+    /**
+     * This checks if a user is logged in or not depending on if userID is -1 or an actual value
+     * @return the respective boolean if user is logged in or not and is stored in static var loginChecker
+     */
     public static boolean isLogin(){
         if (userID!= -1){
             loginChecker = true;
@@ -77,7 +132,31 @@ public class accounts {
         }
         return loginChecker;
     }
+    /**
+     * This checks if the username is already taken.
+     * @param username the username being checked
+     * @return the userID of the given username if taken or -1 otherwise
+     */
+    public static int isIdTaken(String username){
+        String getIDStatment = "select \"userID\" from \"users\" where (username = '" + username + "')";
+        int tempID = -1;
+        try{
+            ResultSet rset = stmt.executeQuery(getIDStatment);
+            while(rset.next()){
+                tempID = rset.getInt("userID");
+            }
 
+        }
+        catch(SQLException e){
+            tempID = -1;
+        }
+        return tempID;
+    }
+
+    /**
+     * This method logs in a user - storing the userID into the static var and updating the last
+     * accessed timestamp.
+     */
     public static void Login(){
         System.out.println("Welcome to login. Please enter credentials below: \n");
         System.out.println("Enter username:");
@@ -96,21 +175,30 @@ public class accounts {
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
             String updateLastAccess = "update users set \"lastAccess\" = '" + currentTime +"' where \"userID\" = " + userID;
             stmt.executeUpdate(updateLastAccess); 
-            isLogin();
-            printMainMenu();
-
+            if(isLogin()){
+                printMainMenu();
+            }
         }
         catch(SQLException e){
             System.out.println("Could not login - try again or create an account");
             printBeginMenu();
         }
     }
+
+    /**
+     * This method is used to logout. Resets userID to -1 and loginchecker to be false'
+     */
     public static void Logout(){
         userID = -1;
         loginChecker = false;
+        System.out.println("You are now logged out and disconnected, goodbye ;)");
     }
-
+    
+    /**
+     * This method is used to create a new account and prompts user for everything but userID which is generated
+     */
     public static void createAccount(){
+        // incrementCounterUserID();
         System.out.println("Welcome to Account Creation. Please enter credentials below: \n");
         System.out.println("Please enter first name:");
         String firstName = sc.nextLine();
@@ -118,6 +206,13 @@ public class accounts {
         String lastName = sc.nextLine();
         System.out.println("Please enter new username:");
         String username = sc.nextLine();
+        //check if username taken checkerID will not be -1
+        int checkerID = isIdTaken(username);
+        while(checkerID != -1){
+            System.out.println("Username taken - please enter new username:");
+            username = sc.nextLine();
+            checkerID = isIdTaken(username);
+        }
         System.out.println("Please enter new password:");
         String password = sc.nextLine();
         Timestamp currentTime = new Timestamp(System.currentTimeMillis());
@@ -125,17 +220,24 @@ public class accounts {
         System.out.println("SQL IS "+ insertNewUser);
         try {
             stmt.executeUpdate(insertNewUser);
-            incrementUserID++;
             System.out.println("Your account has been created, please sign in to see other functionality");
             printBeginMenu();
+            Accounts.incrementUserID+=1;
+            BufferedWriter bw = new BufferedWriter(new FileWriter("src/incrementUserID.txt",false));
+            bw.write(incrementUserID);
         } catch (SQLException e) {
-            // prompt to reenter new username
             e.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
         }
 
         
-
     }
+    /**
+     * Temp main - to be removed.
+     * @param args
+     * @throws SQLException
+     */
     public static void main(String[] args) throws SQLException {
 
         int lport = 5432;
@@ -149,8 +251,7 @@ public class accounts {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        // String user = cs_user; //change to your username
-        // String password = cs_password; //change to your password
+      
 
         String databaseName = "p320_16"; //change to your database name
 
@@ -183,12 +284,11 @@ public class accounts {
             System.out.println("Database connection established");
 
             stmt = conn.createStatement();
-            // createAccount();
-            // Login();
-            // System.out.println(isLogin());
-            System.out.println(incrementUserID);
+            Accounts acc = new Accounts();
+            // System.out.println(incrementUserID);
             printBeginMenu();
-            
+
+            sc.close();
 
             // Do something with the database....
 
